e74f1362b003828e41ecf2b4f89a688dd5b0b80d
Frederik Rüütel <kfreqk@gmail.com>
2026-09-02 13:21:10 +0300

    Variables

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-echo "Hello, World!";
+echo "Hello, World!"; // Prints Hello World message
+
+$name = "Frederik"; // Variables käib $ sümboliga
+$age = 19;
+
+echo " $age";
+$age = $age * 2 * 3 * 5 * 4; // Update the age variable with multipliers
+echo " $age"; 
 ?>
