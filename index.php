5e6d38a24547536091281423eeba9a30025f4ac7
Frederik Rüütel <kfreqk@gmail.com>
2026-09-04 13:50:38 +0300

    add boolean math

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
-$test = 'hello';
-$test = 'hello' . ' world';
+$test = true;
+$test = false;
+$test = true && false;
 var_dump($test);
 
-$test .= '!';
+$test = true && true; // kui koik bool'id on true, siis and and on true. Kui on false sees siis on false.
 var_dump($test);
 
-$name = "Frederik";
-$age = 19;
-$test = $name . ' is ' . $age . ' years old.';
+$test = true || false; // true or false, kui vahemalt 1 osapooltest on toene
 var_dump($test);
 
-$test = 'hello\n\nworld'; // Topelt jutumarkides tootavad variableid
-$test = "hello\n\nworld";
+$test = !false; // not false
 var_dump($test);
 
-$letter = 'a';
-$letter++;
-var_dump($letter);
+$test = 10 > 5; // kui on suurem kui, siis on true, ehk kui 10 on suurem kui 5 siis on true, kui on vaiksem siis false
+var_dump($test);
 
+$test = 10 < 5;
+var_dump($test);
+
+$test = 10 > 10;
+$test = 10 == 10;
+$test = 10 === 10;
+$test = 10 >= 10;
+$test = 10 <= 10;
+$test = 10 != 5;
+$test = 10 !== 5;
+$test = 10 == '10'; # kontrollib lihtsalt. Tuleb true
+$test = 10 === '10'; # range kontroll. Tuleb false
+var_dump($test);
+
+$test = true && false || (true && !true);
+var_dump($test);
 ?>
