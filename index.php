84571f68a5d6b22ec8ecbaa0b03aad013ad46a1e
Frederik Rüütel <kfreqk@gmail.com>
2026-09-04 14:06:25 +0300

    add array examples

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
-$test = true;
-$test = false;
-$test = true && false;
+$test = [1, 2, 'word', true];
+var_dump($test);
+var_dump($test[2]); ## kirjutab ainult 2. kohal oleva
+
+$test[2] = 'nigga'; ## saab muuta array's asju
+
+$test[4] = 'new value'; ## lisab uue asja array'sse
+
+$test[] = 'another value'; # lisab loppu juurde
+var_dump($test);
+array_push($test, 'pushed value'); ## saab samoodi lõppu lisada
 var_dump($test);
 
-$test = true && true; // kui koik bool'id on true, siis and and on true. Kui on false sees siis on false.
+$test = ['name' => 'Frederik', 'age' => 19]; ## array saab ka nii teha
+
+# voib treppida seda kui array laheb pikaks.
+$test = [
+            'name' => 'Frederik', 
+            'age' => 19,
+            1,
+            3,
+            'black men',
+            100 => 'new value',
+            5, # vaatab mis on viimane numbriline voti, hetkel on jargmine 101
+        ];
+
+var_dump($test['name']);
 var_dump($test);
 
-$test = true || false; // true or false, kui vahemalt 1 osapooltest on toene
-var_dump($test);
+$test = [
+    [1, 2, 3], # kahemootmeline array
+    [4, 5, 6],
+    [7, 8, 9],
+];
 
-$test = !false; // not false
-var_dump($test);
-
-$test = 10 > 5; // kui on suurem kui, siis on true, ehk kui 10 on suurem kui 5 siis on true, kui on vaiksem siis false
-var_dump($test);
-
-$test = 10 < 5;
-var_dump($test);
-
-$test = 10 > 10;
-$test = 10 == 10;
-$test = 10 === 10;
-$test = 10 >= 10;
-$test = 10 <= 10;
-$test = 10 != 5;
-$test = 10 !== 5;
-$test = 10 == '10'; # kontrollib lihtsalt. Tuleb true
-$test = 10 === '10'; # range kontroll. Tuleb false
-var_dump($test);
-
-$test = true && false || (true && !true);
-var_dump($test);
+var_dump($test[1][2])
 ?>
