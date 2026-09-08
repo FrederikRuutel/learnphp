cc6a0aed6823ce142b17ca17aa8e9b141472314a
Frederik Rüütel <kfreqk@gmail.com>
2026-09-08 11:12:31 +0300

    add if and switch cases

--- index.php
+++ index.php
@@ -1,38 +1,44 @@
 <?php
-$test = [1, 2, 'word', true];
-var_dump($test);
-var_dump($test[2]); ## kirjutab ainult 2. kohal oleva
+$num = 10;
+if($num > 5) {
+    var_dump('Bigger');
+}
 
-$test[2] = 'nigga'; ## saab muuta array's asju
+if($num > 5) {
+    var_dump('Bigger');
+} else {
+    var_dump('Smaller');
+}
 
-$test[4] = 'new value'; ## lisab uue asja array'sse
 
-$test[] = 'another value'; # lisab loppu juurde
-var_dump($test);
-array_push($test, 'pushed value'); ## saab samoodi lõppu lisada
-var_dump($test);
+if($num > 5) {
+    var_dump('Bigger');
+} else if($num < 5) {
+    var_dump('Smaller');
+} else {
+    var_dump('Equal');
+}
 
-$test = ['name' => 'Frederik', 'age' => 19]; ## array saab ka nii teha
 
-# voib treppida seda kui array laheb pikaks.
-$test = [
-            'name' => 'Frederik', 
-            'age' => 19,
-            1,
-            3,
-            'black men',
-            100 => 'new value',
-            5, # vaatab mis on viimane numbriline voti, hetkel on jargmine 101
-        ];
-
-var_dump($test['name']);
-var_dump($test);
-
-$test = [
-    [1, 2, 3], # kahemootmeline array
-    [4, 5, 6],
-    [7, 8, 9],
-];
-
-var_dump($test[1][2])
+$i = 1;
+switch($i) {
+    case 1:
+        var_dump('yx');
+        break;
+    case 2:
+        var_dump('Kaks');
+        break;
+    case 3:
+    case 4:
+        var_dump('kolm neli');
+        break;
+    case 5:
+        var_dump('viis');
+    case 6:
+        var_dump('kuus');
+        break;
+    default:
+        var_dump('dunno');
+    
+}
 ?>
