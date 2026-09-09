e242d87dd2d843c4283e6537f6dcf82b932e98a7
Frederik Rüütel <kfreqk@gmail.com>
2026-09-09 13:44:10 +0300

    Add objects

--- index.php
+++ index.php
@@ -20,3 +20,26 @@ class Box {
         return $this->height * $this->length * $this->width;
     }
 }
+
+$num1 = 1;
+$num2 = $num1;
+$num1 = 2;
+var_dump($num1, $num2);
+
+$box1 = new Box();
+$box1->width = 1;
+$box2 = clone $box1;
+$box2->width = 2;
+var_dump($box1, $box2);
+var_dump($box1->width, $box2->width);
+
+
+class MetalBox extends Box {
+    public $weight;
+    public function mass(){
+        return $this->volume() * $this->weight;
+    }
+}
+
+$metal1 = new MetalBox();
+var_dump($metal1);
