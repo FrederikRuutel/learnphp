deec591374a3e47e3ef8f47542f7edd4b71c14c4
Frederik Rüütel <kfreqk@gmail.com>
2026-09-09 10:04:33 +0300

    Add Class Example

--- index.php
+++ index.php
@@ -1,44 +1,22 @@
 <?php
 
-function hello() {
-    var_dump('Hello World!');
+class Box {
+    public $width;
+    public $height;
+    public $length;
+    public $isOpen = false;
+    public $hasBeenOpened = false;
+    
+    public function open() {
+        $this->isOpen = true;
+        $this->hasBeenOpened = true;
+    }
+
+    public function close() {
+        $this->isOpen = false;
+    }
+
+    public function volume() {
+        return $this->height * $this->length * $this->width;
+    }
 }
-
-hello();
-hello();
-hello();
-
-function helloName($name) {
-    var_dump("Hello, $name!");
-}
-
-helloName('Frederik');
-
-function helloNameAndAge($name, $age) {
-    var_dump("Hello, $name, who is $age yo.");
-}
-
-helloNameAndAge('Frederik', '19');
-helloNameAndAge('karl', 18);
-
-$test = function() {
-    echo("Tere!");
-};
-
-var_dump($test);
-
-$numbers = [1,2,3,4,5];
-$squares = array_map(function ($n) {
-    return $n * $n;
-}, $numbers);
-var_dump($squares);
-
-function cube($a) {
-    return $a * $a * $a;
-};
-var_dump(cube(4));
-
-$answer = cube(5);
-
-$text = "Cubes of 5 is $answer";
-echo $text;
