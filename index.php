c1bdffc361d65138b9d0ada2faeeaf8560acc662
Frederik Rüütel <kfreqk@gmail.com>
2026-09-09 13:49:36 +0300

    Add traits example

--- index.php
+++ index.php
@@ -1,45 +1,23 @@
 <?php
 
-class Box {
-    public $width;
-    public $height;
-    public $length;
-    public $isOpen = false;
-    public $hasBeenOpened = false;
-    
-    public function open() {
-        $this->isOpen = true;
-        $this->hasBeenOpened = true;
-    }
+class Cat {
+    use makesSound;
+}
 
-    public function close() {
-        $this->isOpen = false;
-    }
+class Dog {
+    use HasSmell, makesSound;
+}
 
-    public function volume() {
-        return $this->height * $this->length * $this->width;
+trait hasSmell {
+    public $smell;
+    public function sniff() {
+        return 'Smells like ' . $this->smell;
     }
 }
 
-$num1 = 1;
-$num2 = $num1;
-$num1 = 2;
-var_dump($num1, $num2);
-
-$box1 = new Box();
-$box1->width = 1;
-$box2 = clone $box1;
-$box2->width = 2;
-var_dump($box1, $box2);
-var_dump($box1->width, $box2->width);
-
-
-class MetalBox extends Box {
-    public $weight;
-    public function mass(){
-        return $this->volume() * $this->weight;
+trait makesSound {
+    public $sound;
+    public function noise() {
+        return $this->sound;
     }
 }
-
-$metal1 = new MetalBox();
-var_dump($metal1);
