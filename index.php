50a9923f831173978e1500c6cd4497105bc7a81e
Frederik Rüütel <kfreqk@gmail.com>
2026-09-11 13:49:26 +0300

    Add magic methods

--- index.php
+++ index.php
@@ -1,23 +1,33 @@
 <?php
 
-class Cat {
-    use makesSound;
-}
+class Box {
 
-class Dog {
-    use HasSmell, makesSound;
-}
+    public function __construct(private $w, private $h, private $l) {
+        var_dump('Box created');
+    }
 
-trait hasSmell {
-    public $smell;
-    public function sniff() {
-        return 'Smells like ' . $this->smell;
+    public function volume() {
+        return $this->w * $this->h * $this->l;
+    }
+
+    public function __set($name, $value) {
+        $this->$name = $value;
+        var_dump($name, $value);
+    }
+
+    public function __toString() {
+        return "Box: {$this->w} x {$this->h} x {$this->l}";
+    }
+
+    public function __destruct() {
+        var_dump('Box destroyed');
     }
 }
 
-trait makesSound {
-    public $sound;
-    public function noise() {
-        return $this->sound;
-    }
-}
+$box1 = new Box(1, 2, 3);
+$box1->color = 'red';
+$box1 == 1;
+$box2 = new Box(4, 5, 6);
+var_dump($box1, $box2);
+$box3 = clone $box2;
+echo $box1;
