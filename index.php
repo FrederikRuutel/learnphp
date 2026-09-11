90c8b0917147f642bd9dda56653f931e1e2a25d1
Frederik Rüütel <kfreqk@gmail.com>
2026-09-11 14:33:39 +0300

    loggers

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
 <?php
 
-class Box {
-
-    public function __construct(private $w, private $h, private $l) {
-        var_dump('Box created');
-    }
-
-    public function volume() {
-        return $this->w * $this->h * $this->l;
-    }
-
-    public function __set($name, $value) {
-        $this->$name = $value;
-        var_dump($name, $value);
-    }
-
-    public function __toString() {
-        return "Box: {$this->w} x {$this->h} x {$this->l}";
-    }
-
-    public function __destruct() {
-        var_dump('Box destroyed');
+class Job {
+    public function task(ConsoleLogger $logger) {
+        for($i=0; $i<10; $i++) {
+            $logger->log("Task running: " . $i);
+        }
     }
 }
 
-$box1 = new Box(1, 2, 3);
-$box1->color = 'red';
-$box1 == 1;
-$box2 = new Box(4, 5, 6);
-var_dump($box1, $box2);
-$box3 = clone $box2;
-echo $box1;
+class ConsoleLogger {
+    public function log($message) {
+        echo $message;
+    }
+}
+
+class NothingLogger {
+    public function log($message) {
+        // Do nothing
+    }
+}
+
+$job = new Job();
+$logger = new NothingLogger();
+$job->task($logger);
