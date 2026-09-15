110fb041a13b4b4717425acf40567e95c257931d
Frederik Rüütel <kfreqk@gmail.com>
2026-09-15 11:28:57 +0300

    add views folder, public index php

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false; // serve the requested resource as-is.
+}
+
+function dump(...$var) {
+    echo '<pre>';
+    var_dump(...$var);
+    echo '</pre>';
+}
+
+switch ($_SERVER['REQUEST_URI']) {
+    case '/':
+        include __DIR__ . '/../views/index.php';
+        break;
+    case '/us':
+        include __DIR__ . '/../views/us.php';
+        break;
+    default:
+        echo '404 Not Found';
+}
